47462030db647abaed88a69beda2cbe853965e52
Gabriel Luppi <luppi.gabriel08@gmail.com>
2022-07-12 17:41:22 -0300

    Documentacao endereco

--- modulo3/pessoa-api/src/main/java/br/com/luppi/pessoaapi/controller/EnderecoController.java
+++ modulo3/pessoa-api/src/main/java/br/com/luppi/pessoaapi/controller/EnderecoController.java
@@ -4,6 +4,9 @@ import br.com.luppi.pessoaapi.dto.EnderecoCreateDTO;
 import br.com.luppi.pessoaapi.dto.EnderecoDTO;
 import br.com.luppi.pessoaapi.exception.RegraDeNegocioException;
 import br.com.luppi.pessoaapi.service.EnderecoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -20,33 +23,80 @@ public class EnderecoController {
     private EnderecoService enderecoService;
 
 
+    @Operation(summary = "Cadastrar novo endereço")
+        @ApiResponses(
+                value = {
+                        @ApiResponse(responseCode = "200", description = "Retorna o endereço criado"),
+                        @ApiResponse(responseCode = "400", description = "Dados inválidos"),
+                        @ApiResponse(responseCode = "500", description = "Exception gerada")
+                }
+        )
     @PostMapping("/{idPessoa}")
     public ResponseEntity<EnderecoDTO> create(@PathVariable("idPessoa") Integer id,
                                               @RequestBody @Valid EnderecoCreateDTO endereco) throws RegraDeNegocioException {
         return ResponseEntity.ok(enderecoService.create(id, endereco));
     }
 
+    @Operation(summary = "Listar endereços cadastrados")
+        @ApiResponses(
+                value = {
+                        @ApiResponse(responseCode = "200", description = "Retorna toda a lista de endereços"),
+                        @ApiResponse(responseCode = "500", description = "Exception gerada")
+                }
+        )
     @GetMapping
     public ResponseEntity<List<EnderecoDTO>>  list() {
         return ResponseEntity.ok(enderecoService.list());
     }
 
+    @Operation(summary = "Listar endereço por ID do endereço")
+        @ApiResponses(
+                value = {
+                        @ApiResponse(responseCode = "200", description = "Retorna o contato através do ID"),
+                        @ApiResponse(responseCode = "400", description = "{idEndereco} inválido"),
+                        @ApiResponse(responseCode = "500", description = "Exception gerada")
+                }
+        )
     @GetMapping("/{idEndereco}")
     public ResponseEntity<List<EnderecoDTO>> listByAddressId(@PathVariable("idEndereco") Integer id) throws  RegraDeNegocioException{
         return ResponseEntity.ok(enderecoService.listByAddressId(id));
     }
 
+    @Operation(summary = "Listar endereços por ID da pessoa")
+        @ApiResponses(
+                value = {
+                        @ApiResponse(responseCode = "200", description = "Retorna a lista de endereços por idPessoa"),
+                        @ApiResponse(responseCode = "400", description = "{idPessoa} inválido"),
+                        @ApiResponse(responseCode = "500", description = "Exception gerada")
+                }
+        )
     @GetMapping("/{idPessoa}/pessoa")
     public ResponseEntity<List<EnderecoDTO>>  listByPersonId(@PathVariable("idPessoa") Integer id) throws RegraDeNegocioException {
         return ResponseEntity.ok(enderecoService.listByPersonId(id));
     }
 
+    @Operation(summary = "Atualizar endereço através do {idEndereco}")
+        @ApiResponses(
+                value = {
+                        @ApiResponse(responseCode = "200", description = "Endereço atualizado com sucesso"),
+                        @ApiResponse(responseCode = "400", description = "{idEndereco} inválido"),
+                        @ApiResponse(responseCode = "500", description = "Exception gerada")
+                }
+        )
     @PutMapping("/{idEndereco}")
     public ResponseEntity<EnderecoDTO> uptade(@PathVariable("idEndereco") Integer id,
                                  @RequestBody @Valid EnderecoCreateDTO enderecoAtualizado) throws RegraDeNegocioException {
         return ResponseEntity.ok(enderecoService.update(id, enderecoAtualizado));
     }
 
+    @Operation(summary = "Deletar endereço através do {idEndereco}")
+        @ApiResponses(
+                value = {
+                        @ApiResponse(responseCode = "200", description = "Endereço deletado com sucesso"),
+                        @ApiResponse(responseCode = "400", description = "{idEndereco} inválido"),
+                        @ApiResponse(responseCode = "500", description = "Exception gerada")
+                }
+        )
     @DeleteMapping("/{idEndereco}")
     public void delete(@PathVariable("idEndereco") Integer id) throws RegraDeNegocioException {
         enderecoService.delete(id);
--- modulo3/pessoa-api/src/main/java/br/com/luppi/pessoaapi/dto/EnderecoCreateDTO.java
+++ modulo3/pessoa-api/src/main/java/br/com/luppi/pessoaapi/dto/EnderecoCreateDTO.java
@@ -1,6 +1,7 @@
 package br.com.luppi.pessoaapi.dto;
 
 import br.com.luppi.pessoaapi.enums.TipoEndereco;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
@@ -11,21 +12,30 @@ import javax.validation.constraints.Size;
 public class EnderecoCreateDTO {
 
     private Integer idPessoa;
+
+    @Schema(description = "Tipo do endereço")
     @NotNull(message = "Insira um tipo para o endereco")
     private TipoEndereco tipo;
+
     @Size(min=3, max=250)
     @NotBlank(message = "Informe um logradouro")
     private String logradouro;
+
     @NotNull(message = "Informe um numero")
     private Integer numero;
+
+    @Schema(description = "CEP apenas números")
     @Size(min=8, max=8)
     @NotBlank(message = "Informe um CEP valido")
     private String cep;
+
     @Size(min=2, max=250)
     @NotBlank(message = "Informe uma cidade")
     private String cidade;
+
     @NotBlank
     private String estado;
+
     @NotBlank
     private String pais;
 
