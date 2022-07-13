9369d06df0e3f406b8151cf006251e17142c21d2
Gabriel Luppi <luppi.gabriel08@gmail.com>
2022-07-12 23:25:40 -0300

    Adicionando documentacao do Contato

--- modulo3/pessoa-api/src/main/java/br/com/luppi/pessoaapi/dto/ContatoCreateDTO.java
+++ modulo3/pessoa-api/src/main/java/br/com/luppi/pessoaapi/dto/ContatoCreateDTO.java
@@ -1,6 +1,7 @@
 package br.com.luppi.pessoaapi.dto;
 
 import br.com.luppi.pessoaapi.enums.TipoContato;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
@@ -9,13 +10,14 @@ import javax.validation.constraints.Size;
 
 @Data
 public class ContatoCreateDTO {
-
-    private Integer idContato;
+    @Schema(description = "Indentificador do dono de contato")
     private Integer idPessoa;
 
     @NotNull(message = "Tipo do contato nao pode ser nulo")
     private TipoContato tipoContato;
     @Size(min=3, max=13)
+
+    @Schema(description = "Permitido caracteres especiais: ( )")
     @NotBlank(message = "Numero nao pode ser nulo ou em branco")
     private String telefone;
     @NotBlank(message = "Descricao nao pode ser nulo ou em branco")
