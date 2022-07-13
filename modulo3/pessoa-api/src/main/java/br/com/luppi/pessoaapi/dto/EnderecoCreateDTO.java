a90af4094b1825da0cb51d6d08f5b7ccfe0b36bf
Gabriel Luppi <luppi.gabriel08@gmail.com>
2022-07-13 10:20:20 -0300

    Arrumando examples dos DTO's

--- modulo3/pessoa-api/src/main/java/br/com/luppi/pessoaapi/dto/EnderecoCreateDTO.java
+++ modulo3/pessoa-api/src/main/java/br/com/luppi/pessoaapi/dto/EnderecoCreateDTO.java
@@ -11,31 +11,33 @@ import javax.validation.constraints.Size;
 @Data
 public class EnderecoCreateDTO {
 
-    @Schema(description = "Residente ou proprietário do local")
+    @Schema(description = "Identificador do residente ou proprietário do local")
     private Integer idPessoa;
 
-    @Schema(description = "Tipo do endereço")
+    @Schema(description = "Tipo do endereço", example = "COMERCIAL")
     @NotNull(message = "Insira um tipo para o endereco")
     private TipoEndereco tipo;
 
-    @Schema(description = "Nome da rua/avenida")
+    @Schema(description = "Nome da rua/avenida", example = "Rua Deputado Álvares Florence")
     @Size(min=3, max=250)
     @NotBlank(message = "Informe um logradouro")
     private String logradouro;
 
+    @Schema(example = "1302")
     @NotNull(message = "Informe um numero")
     private Integer numero;
 
-    @Schema(description = "CEP apenas números", example = "87508-065 -> inválido | 23033180 -> válido")
+    @Schema(description = "CEP de apenas números", example = "09890530")
     @Size(min=8, max=8)
     @NotBlank(message = "Informe um CEP valido")
     private String cep;
 
+    @Schema(example = "São Bernardo do Campo")
     @Size(min=2, max=250)
     @NotBlank(message = "Informe uma cidade")
     private String cidade;
 
-    @Schema(description = "Permitido abreviação ou nome completo", example = "SP ou São Paulo")
+    @Schema(description = "Permitido abreviação ou nome completo", example = "SP")
     @NotBlank
     private String estado;
 
