bcd63b17d57e693882c72ef147da27083d82952a
Gabriel Luppi <luppi.gabriel08@gmail.com>
2022-08-04 09:10:38 -0300

    Adicionando id de cliente e pet nos inserts

--- modulo4.1/homework_1.js
+++ modulo4.1/homework_1.js
@@ -1,12 +1,12 @@
 db.pedidos.insertOne(
-    { _id: 0, servico: "banho", valor: 90, descricao: "SD", status: "ABERTO", data: "2022-08-04" }
+    { _id: 0, id_cliente: 1, id_pet: 1, servico: "banho", valor: 90, descricao: "SD", status: "ABERTO", data: "2022-08-04" }
 )
 
 db.pedidos.insertMany(
-    { _id: 1, servico: "tosa", valor: 45, descricao: "Pelo fragilizado", status: "EM_ANDAMENTO", data: "2022-08-05" },
-    { _id: 2, servico: "corte de unha", valor: 15, descricao: "SD", status: "CONCLUIDO", data: "2022-08-02" },
-    { _id: 3, servico: "banho", valor: 60, descricao: "SD", status: "CONCLUIDO", data: "2022-08-02" },
-    { _id: 4, servico: "adestramento", valor: 150, descricao: "arisco com desconhecidos", status: "CANCELADO", data: "2022-08-03" }
+    { _id: 1, id_cliente: 2, id_pet: 2, servico: "tosa", valor: 45, descricao: "Pelo fragilizado", status: "EM_ANDAMENTO", data: "2022-08-05" },
+    { _id: 2, id_cliente: 5, id_pet: 5, servico: "corte de unha", valor: 15, descricao: "SD", status: "CONCLUIDO", data: "2022-08-02" },
+    { _id: 3, id_cliente: 2, id_pet: 2, servico: "banho", valor: 60, descricao: "SD", status: "CONCLUIDO", data: "2022-08-02" },
+    { _id: 4, id_cliente: 7, id_pet: 9, servico: "adestramento", valor: 150, descricao: "arisco com desconhecidos", status: "CANCELADO", data: "2022-08-03" }
 )
 
 db.pedidos.find({status: "CONCLUIDO"})
